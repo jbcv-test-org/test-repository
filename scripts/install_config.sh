5c9dcab39ad198f5e0684e1d07d27b9960b0372f
Jo Basevi <johanna.basevi@anu.edu.au>
2024-12-17 14:56:23 +1100

    Testing payu 1.1.6

--- scripts/install_config.sh
+++ scripts/install_config.sh
@@ -1,21 +1,40 @@
 ### Settings to control installation path e.g. for test installs
+
+# Environment that sets the base of paths for apps and modules directories
 if [[ ! "${CONDA_BASE}" ]]; then
     echo "Error! CONDA_BASE must be defined"
     exit 1
 fi
+# Admin directory to store PBS logs and stage tar files of built environments
 if [[ ! "${ADMIN_DIR}" ]]; then
     echo "Error! ADMIN_DIR must be defined"
     exit 1
 fi
 
+# Temporary path to build conda environments on PBS jobs
 export CONDA_TEMP_PATH="${PBS_JOBFS:-${CONDA_TEMP_PATH}}"
+
+# Script directory path that contains this repository, used to source and 
+# copy across scripts and the base container image if built
 export SCRIPT_DIR="${SCRIPT_DIR:-$PWD}"
 
+# SCRIPT_SUBDIR contains the environment launcher scripts (for every file on 
+# $PATH inside the squashfs environment) that launches a container and 
+# runs commands inside the containerised environment
 export SCRIPT_SUBDIR="apps/conda_scripts"
 export MODULE_SUBDIR="modules"
 export APPS_SUBDIR="apps"
-export CONDA_INSTALL_BASENAME="base_conda" #TODO: Replace with conda?
-export MODULE_NAME="conda_container" # TODO: Replace with conda?
+
+# CONDA_INSTALL_BASENAME is a sub-directory in apps that contains the micromamba
+# install and the envs sub-directory that contains a squashfs file for each
+# environment and symlinks to in-container conda environment paths
+export CONDA_INSTALL_BASENAME="base_conda" #TODO: Eventually replace with conda
+
+# Modules are named $MODULE_NAME/$MODULE_VERSION where $MODULE_VERSION can
+# be $ENVIRONMENT-$VERSION, (e.g. conda/analysis3-24.07)
+# Note: this is currently over-ridden in the payu environment config
+# so payu modules are named payu/$MODULE_VERSION (e.g. payu/1.1.5)
+export MODULE_NAME="conda_container" # TODO: Eventually replace with conda
 
 ### Derived locations - extra '.' for arcane rsync magic
 export CONDA_SCRIPT_PATH="${CONDA_BASE}"/./"${SCRIPT_SUBDIR}"
@@ -49,10 +68,13 @@ if [[ "${CONDA_ENVIRONMENT}" ]]; then
         exit 1
     fi
 
+    # FULLENV is the name used for environment's launcher script subdirectory,
+    # squashfs file name, and name of the environment inside of the container
     if [[ ! "${FULLENV}" ]]; then
         echo "Error! FULLENV must be defined in ${CONDA_ENVIRONMENT} config file"
         exit 1
     fi
+    # MODULE_VERSION is the version name used for the modulefile
     if [[ ! "${MODULE_VERSION}" ]]; then
         echo "Error! MODULE_VERSION must be defined in ${CONDA_ENVIRONMENT} config file"
         exit 1
