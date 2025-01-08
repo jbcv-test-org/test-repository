1338037c7ae404ec60261c4437a54c29892af9ec
Jo Basevi <johanna.basevi@anu.edu.au>
2025-01-08 15:27:54 +1100

    Allow payu/dev module names

--- environments/payu-dev/config.sh
+++ environments/payu-dev/config.sh
@@ -1,5 +1,5 @@
 ### config.sh MUST provide the following:
-### $FULLENV
+### $FULLENV $MODULE_VERSION
 ###
 ### Arrays used by the build system (optional, can be empty)
 ### rpms_to_remove
@@ -8,8 +8,20 @@
 ### outside_files_to_copy
 
 ### Version settings
-export ENVIRONMENT=payu-dev
-export FULLENV="${ENVIRONMENT}"
+# Note: VERSION_TO_MODIFY gets set in the custom build and deploy scripts
+# Check does dev conflict with the payu-dev symlink?
+export VERSION_TO_MODIFY=dev
+
+### Version settings
+export ENVIRONMENT=payu
+export FULLENV="${ENVIRONMENT}-${VERSION_TO_MODIFY}"
+export MODULE_VERSION="${VERSION_TO_MODIFY}"
+
+# Override general module path settings to name modulefiles payu/$VERSION
+# in build scripts and custom deploy script
+export MODULE_NAME="${ENVIRONMENT}"
+export MODULE_PATH="${CONDA_BASE}"/./"${MODULE_SUBDIR}"
+export CONDA_MODULE_PATH="${MODULE_PATH}"/"${MODULE_NAME}"
 
 declare -a rpms_to_remove=()
 declare -a replace_from_apps=()
