13ce050cd8b19b20461993a92fddbb1a78a812cd
Jo Basevi <johanna.basevi@anu.edu.au>
2025-03-06 11:55:34 +1100

    Add activation script to container runscript

--- scripts/build.sh
+++ scripts/build.sh
@@ -113,6 +113,16 @@ function inner() {
     done
     popd
 
+    ### Set up environment activate script
+    cat > "${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/bin/activate" <<EOF
+export PATH=${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/bin:\$PATH
+for config in "${CONDA_INSTALLATION_PATH}/envs/${FULLENV}/etc/conda/activate.d/*.sh; do
+    if [ -r "\$config" ]; then
+        source "\$config"
+    fi
+done
+EOF
+
     ### Update any supporting infrastructure
     copy_if_changed "${SCRIPT_DIR}"/launcher.sh "${CONDA_SCRIPT_PATH}"/launcher.sh
     for override in "${SCRIPT_DIR}"/overrides/*; do
@@ -248,7 +258,7 @@ ln -s /opt/conda/"${FULLENV}" "${CONDA_OUTER_BASE}"/"${APPS_SUBDIR}"/"${CONDA_IN
 
 ### Can't use ${CONDA_SCRIPT_PATH} or "${CONDA_INSTALLATION_PATH}" due to the need to string match on those paths
 ### which they won't with the '/./' part required for arcane rsync magic
-construct_module_insert "${SINGULARITY_BINARY_PATH}" "${OVERLAY_BASE}" "${my_container}" "${BUILD_STAGE_DIR}"/"${FULLENV}".sqsh.tmp "${SCRIPT_DIR}"/condaenv.sh "${CONDA_INSTALLATION_PATH}" /opt/conda/"${FULLENV}" "${CONDA_BASE}"/"${SCRIPT_SUBDIR}"/"${FULLENV}".d/bin "${CONDA_OUTER_BASE}"/"${MODULE_SUBDIR}"/"${MODULE_NAME}"/."${MODULE_VERSION}"
+# construct_module_insert "${SINGULARITY_BINARY_PATH}" "${OVERLAY_BASE}" "${my_container}" "${BUILD_STAGE_DIR}"/"${FULLENV}".sqsh.tmp "${SCRIPT_DIR}"/condaenv.sh "${CONDA_INSTALLATION_PATH}" /opt/conda/"${FULLENV}" "${CONDA_BASE}"/"${SCRIPT_SUBDIR}"/"${FULLENV}".d/bin "${CONDA_OUTER_BASE}"/"${MODULE_SUBDIR}"/"${MODULE_NAME}"/."${MODULE_VERSION}"
 
 ### Set permissions on base environment
 set_apps_perms "${CONDA_OUTER_BASE}"
